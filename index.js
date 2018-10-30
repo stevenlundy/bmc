316e2f196309e12281cc1fd9c00e8fc08e551a7f
Steven Lundy <steven.lundy@gmail.com>
2018-10-29 17:42:55 -0700

    Initial set up of segment player

---
+++ index.js
@@ -0,0 +1,37 @@
+let getEpisodeNumber = function(episode) {
+  let pattern = /^#([0-9]+) .*$/;
+  try {
+    return episode.title.match(pattern)[1];
+  } catch (e) {
+    return null;
+  }
+};
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    feedUrl: "http://feeds.gimletmedia.com/hearreplyall?format=xml",
+    episodes: []
+  },
+  created: function() {
+    // Note: some RSS feeds can't be loaded in the browser due to CORS security.
+    // To get around this, you can use a proxy.
+    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
+
+    let component = this;
+    let parser = new RSSParser();
+    parser.parseURL(CORS_PROXY+this.feedUrl, function(err, feed) {
+      feed.items.sort((a, b) => a.pubDate < b.pubDate);
+      let allEpisodes = {};
+      feed.items.forEach(function(episode) {
+        let episodeNumber = getEpisodeNumber(episode);
+        allEpisodes[episodeNumber] = episode;
+      })
+      component.episodes = breakmasterCylinderSegments.map(function(segment) {
+        let episode = allEpisodes[segment.episode];
+        episode.breakmasterCylinderSegmentDuration = segment.duration;
+        return episode;
+      })
+    })
+  }
+})
